c17fa6809f96033bac6bf388e3401f86ba37e5eb
Jeff Webster <chryosolo@users.noreply.github.com>
2017-06-04 10:50:27 -0700

    Update wt1.js

--- wt1.js
+++ wt1.js
@@ -1,9 +1,5 @@
-"use latest";
-
-import util from "util";
-
-//var util = require('util');
+var util = require('util');
 module.exports = function(ctx, cb) {
-    console.log(util.inspect(ctx.body.issue.url));
+    console.log(util.inspect(ctx.body, { depth:null }));
     cb();
 };
