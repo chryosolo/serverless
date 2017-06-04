78d02fdb17d87c9f2b5c7abdd651bdbd9503bca3
Jeff Webster <chryosolo@users.noreply.github.com>
2017-06-04 10:43:20 -0700

    Change printing of body to printing of url

--- wt1.js
+++ wt1.js
@@ -1,5 +1,9 @@
-var util = require('util');
+"use latest";
+
+import util from "util";
+
+//var util = require('util');
 module.exports = function(ctx, cb) {
-    console.log(util.inspect(ctx.body, { depth: null }));
+    console.log(util.inspect(ctx.body.issue.url));
     cb();
 };
